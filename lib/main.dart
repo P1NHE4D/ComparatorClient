226c0a10a511476c4907c5e36668efa93539437d
Marcel Robohm <internetzverbindung@gmail.com>
2020-06-09 15:39:09 +0200

    refactor: naming

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
                 // closer together (more dense) than on mobile platforms.
                 visualDensity: VisualDensity.adaptivePlatformDensity,
             ),
-            home: MyHomePage(title: 'Komparator'),
+            home: MyHomePage(title: 'Comparator'),
         );
     }
 }
