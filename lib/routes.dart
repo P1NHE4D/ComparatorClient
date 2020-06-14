20352515f9c527ae4eb5f113cf5dbc0ac7e41d34
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-14 18:27:16 +0200

    refactor: rename files

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,5 @@
 
-import 'package:comparator/screens/home/homeScreen.dart';
+import 'package:comparator/screens/home/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
