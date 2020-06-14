cf77c80a615b406c44fde9c8a80ce6bc1b2f8479
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-14 17:38:47 +0200

    refactor: add route to home screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,8 @@
 
+import 'package:comparator/screens/home/homeScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-
+  '/': (BuildContext context) => HomeScreen()
 };
