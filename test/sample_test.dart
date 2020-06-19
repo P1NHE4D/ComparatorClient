b7bd8ad3afc256d2a48af765bb7fb82fd501f9c3
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-19 19:43:35 +0200

    refactor: add sample test file

---
+++ test/sample_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:comparator/main.dart';
+
+void main() {
+  testWidgets('Sample test', (WidgetTester tester) async {
+    expect(1, 1);
+  });
+}
