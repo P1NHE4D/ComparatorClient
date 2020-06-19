fb33829e9f809a7823ead4055e0afd1681035e4d
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-19 19:46:03 +0200

    refactor: remove unused imports

--- test/sample_test.dart
+++ test/sample_test.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:comparator/main.dart';
 
 void main() {
   testWidgets('Sample test', (WidgetTester tester) async {
