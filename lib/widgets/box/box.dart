b7145973ea5e712886e3057570f797e2aaa7a5ec
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-24 20:00:51 +0200

    feat: add box widget

---
+++ lib/widgets/box/box.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Box extends StatelessWidget {
+  final Widget title;
+  final List<Widget> children;
+  final Color backgroundColor;
+
+  Box({this.title, this.children, this.backgroundColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.fromLTRB(14, 20, 14, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            title,
+            SizedBox(height: 6),
+            Container(
+                decoration: BoxDecoration(
+                    color: backgroundColor,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    children: children,
+                  ),
+                ))
+          ],
+        ));
+  }
+}
