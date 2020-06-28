f3ecfab746f96fc9976a348ca1840801021377ae
Hafflationist <internetzverbindung@gmail.com>
2020-06-28 18:09:23 +0200

    feat: Com 67 add com progress bar
    * feat: added com_progress_bar
    
    * refactor: added default values
    
    * refactor: removed Container
    
    * refactor: changed default value
    
    * refactor: added @required
    
    * refactor: added @required
    
    * feat: added percent
    
    * feat: added possibility to modify the text color
    
    * feat: percentage is rounded

---
+++ lib/widgets/com_progress_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+
+class ComProgressBar extends StatelessWidget {
+    final Widget title;
+    final Color barColor;
+    final Color textColor;
+
+    final double value;
+    final double height;
+
+    String get percent => (value * 100.0).toStringAsFixed(1);
+
+    ComProgressBar({
+        @required this.title,
+        this.barColor = const Color.fromARGB(0xff, 0x00, 0x00, 0x00),
+        this.textColor = const Color.fromARGB(0xff, 0xff, 0xff, 0xff),
+        @required this.value,
+        this.height = 20.0
+    });
+
+    @override
+    Widget build(BuildContext context) {
+        return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+                title,
+                SizedBox(height: 8),
+                LinearPercentIndicator(
+                    animation: true,
+                    lineHeight: height,
+                    animationDuration: 500,
+                    percent: value,
+                    center: new Text("$percent %", style: TextStyle(color: textColor)),
+                    linearStrokeCap: LinearStrokeCap.roundAll,
+                    progressColor: barColor,
+                    backgroundColor: Color.fromARGB(0xff, 0x18, 0x18, 0x18),
+                )
+            ],
+        );
+    }
+}
