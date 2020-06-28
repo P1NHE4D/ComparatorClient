53f3f85030e860810b94daf125ae2789096ebb75
Marcel Robohm <internetzverbindung@gmail.com>
2020-06-28 17:51:42 +0200

    feat: added possibility to modify the text color

--- lib/widgets/com_progress_bar.dart
+++ lib/widgets/com_progress_bar.dart
@@ -5,6 +5,7 @@ import 'package:percent_indicator/linear_percent_indicator.dart';
 class ComProgressBar extends StatelessWidget {
     final Widget title;
     final Color barColor;
+    final Color textColor;
 
     final double value;
     final double height;
@@ -14,6 +15,7 @@ class ComProgressBar extends StatelessWidget {
     ComProgressBar({
         @required this.title,
         this.barColor = const Color.fromARGB(0xff, 0x00, 0x00, 0x00),
+        this.textColor = const Color.fromARGB(0xff, 0xff, 0xff, 0xff),
         @required this.value,
         this.height = 20.0
     });
@@ -31,7 +33,7 @@ class ComProgressBar extends StatelessWidget {
                     lineHeight: height,
                     animationDuration: 500,
                     percent: value,
-                    center: new Text("$percent %"),     // TODO Anpassende Farbe (Siehe COM-74)
+                    center: new Text("$percent %", style: TextStyle(color: textColor),),     // TODO Anpassende Farbe (Siehe COM-74)
                     linearStrokeCap: LinearStrokeCap.roundAll,
                     progressColor: barColor,
                     backgroundColor: Color.fromARGB(0xff, 0x18, 0x18, 0x18),
