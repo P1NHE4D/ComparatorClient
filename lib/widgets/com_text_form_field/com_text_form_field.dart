5cffaf09bbd92554f22acb05320a07773b9677cc
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-28 14:49:42 +0200

    feat: add text form field widget

---
+++ lib/widgets/com_text_form_field/com_text_form_field.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ComTextFormField extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputAction inputAction;
+  final Function(String) validator;
+  final String label;
+
+  ComTextFormField({@required this.controller, this.validator, this.label, this.inputAction = TextInputAction.done});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      maxLines: 1,
+      textAlignVertical: TextAlignVertical.center,
+      style: TextStyle(color: Colors.white),
+      cursorColor: Colors.white,
+      controller: controller,
+      validator: validator,
+      textInputAction: inputAction,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Color.fromRGBO(82, 82, 82, 1), width: 0)
+          ),
+          filled: true,
+          labelText: label,
+          labelStyle: TextStyle(color : Colors.white70),
+          fillColor: Color.fromRGBO(82, 82, 82, 1),
+          isDense: true,
+          contentPadding: EdgeInsets.all(8),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.white, width: 1)
+          )
+      ),
+    );
+  }
+
+}
