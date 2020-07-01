10938d14d92abc8475366c3a99431d164584a8bc
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-07-01 18:48:26 +0200

    refactor: rename aspect_list_model to query_model

--- lib/models/query_model.dart
+++ lib/models/query_model.dart
@@ -1,8 +1,25 @@
 import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 
-class AspectListModel extends ChangeNotifier {
+class QueryModel extends ChangeNotifier {
+  String _objA = '';
+  String _objB = '';
   final List<String> _aspects = [];
+
+  String get objA => _objA;
+  String get objB => _objB;
+
+  set objA(String obj) {
+    assert(obj != null);
+    _objA = obj;
+    notifyListeners();
+  }
+
+  set objB(String obj) {
+    assert(obj != null);
+    _objB = obj;
+    notifyListeners();
+  }
 
   UnmodifiableListView<String> get aspects => UnmodifiableListView(_aspects);
 
