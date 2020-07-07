24f634051e8639e9977731588f5102b10f586721
Marcel Robohm <internetzverbindung@gmail.com>
2020-07-07 21:18:14 +0200

    feat: added aspect_filter_screen

--- lib/models/emotion_scores.dart
+++ lib/models/emotion_scores.dart
@@ -1,19 +1,27 @@
 class EmotionScores {
-  final num anger;
-  final num disgust;
-  final num fear;
-  final num joy;
-  final num sadness;
+    final num anger;
+    final num disgust;
+    final num fear;
+    final num joy;
+    final num sadness;
 
-  EmotionScores({this.anger, this.disgust, this.fear, this.joy, this.sadness});
+    EmotionScores({this.anger, this.disgust, this.fear, this.joy, this.sadness});
 
-  factory EmotionScores.fromJson(Map<String, dynamic> json) {
-    return EmotionScores(
-      anger: json['anger'],
-      disgust: json['disgust'],
-      fear: json['fear'],
-      joy: json['joy'],
-      sadness: json['sadness']
-    );
-  }
+    factory EmotionScores.fromJson(Map<String, dynamic> json) {
+        if (json == null)
+            return EmotionScores(
+                    anger: 0.0,
+                    disgust: 0.0,
+                    fear: 0.0,
+                    joy: 0.0,
+                    sadness: 0.0
+            );
+        return EmotionScores(
+                anger: json['anger'],
+                disgust: json['disgust'],
+                fear: json['fear'],
+                joy: json['joy'],
+                sadness: json['sadness']
+        );
+    }
 }
--- lib/screens/sentence_list_screen/sentence_list_screen.dart
+++ lib/screens/sentence_list_screen/sentence_list_screen.dart
@@ -1,41 +1,75 @@
+import 'package:comparator/models/query_model.dart';
+import 'package:comparator/screens/aspect_filter_screen/aspect_filter_screen.dart';
 import 'package:comparator/widgets/com_box.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SentenceListScreen extends StatelessWidget {
-  final List<String> sentences;
-  final String title;
+    final List<String> sentences;
+    final String title;
 
-  SentenceListScreen({this.sentences, this.title});
+    SentenceListScreen({this.sentences, this.title});
 
-  Widget _buildListItem(int index) {
-    return ComBox(
-      backgroundColor: Color.fromRGBO(48, 48, 48, 1),
-      child: ListTile(
-        title: Text(
-          sentences[index],
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-    );
-  }
+    Widget _buildListItem(int index, List<String> sentences) =>
+            ComBox(
+                backgroundColor: Color.fromRGBO(48, 48, 48, 1),
+                child: ListTile(
+                    title: Text(
+                        sentences[index],
+                        style: TextStyle(color: Colors.white),
+                    ),
+                ),
+            );
 
-  Widget _buildList() {
-    return ListView.builder(
-        itemBuilder: (context, i) => _buildListItem(i),
-        itemCount: sentences.length);
-  }
+    Widget _buildListInner(List<String> sentences) =>
+            ListView.builder(
+                    itemBuilder: (context, i) => _buildListItem(i, sentences),
+                    itemCount: sentences.length
+            );
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(title, style: TextStyle(color: Colors.white),),
-      ),
-      body: _buildList(),
-    );
-  }
+    Widget _buildList() =>
+            Consumer<QueryModel>(
+                builder: (context, model, child) {
+                    if (!model.enableFiltering)
+                        return _buildListInner(sentences);
 
+                    var aspectsLower = model.aspects.entries
+                            .where((e) => e.value)
+                            .map((e) => e.key)
+                            .map((aspect) => aspect.toLowerCase())
+                            .toList();
+
+                    bool filter(str) => aspectsLower.any((a) => str.toLowerCase().contains(a));
+
+                    var filteredSentences = sentences
+                            .where(filter)
+                            .toList();
+
+                    return _buildListInner(filteredSentences);
+                },
+            );
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+                backgroundColor: Colors.black,
+                title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                        Text(title, style: TextStyle(color: Colors.white),),
+                        IconButton(
+                                icon: Icon(Icons.filter_list),
+                                onPressed: () =>
+                                        Navigator.push(context, MaterialPageRoute(
+                                                builder: (context) => AspectFilterScreen()))
+                        )
+                    ],
+                ),
+            ),
+            body: _buildList(),
+        );
+    }
 }
