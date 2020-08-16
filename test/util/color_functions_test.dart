1acba79f8693a5bdd971cdd23a09535c29dd7cb6
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-08-16 16:43:39 +0200

    test: add color_functions_test

---
+++ test/util/color_functions_test.dart
@@ -0,0 +1,28 @@
+import 'package:comparator/util/color_functions.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+  testWidgets('Color should be red if the progress is 0%', (WidgetTester tester) async {
+    Color c = Color.fromARGB(0xdd, 0xe6, 0x0, 0x0);
+
+    final e = determineBarColor(0, 0, 100);
+    expect(e, c);
+  });
+
+  testWidgets('Color should be yellow if progress is 50%', (WidgetTester tester) async {
+    Color c = Color.fromARGB(0xdd, 0xe6, 0xe6, 0x0);
+
+    final e = determineBarColor(50, 0, 100);
+    expect(e, c);
+  });
+
+  testWidgets('Color should be green if progress is 100%', (WidgetTester tester) async {
+    Color c = Color.fromARGB(0xdd, 0x0, 0xe6, 0x0);
+
+    final e = determineBarColor(100, 0, 100);
+    expect(e, c);
+  });
+}
